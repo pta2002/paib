edb00f3dfce67d345734e770e541525414b055c8
Pedro Alves <pta2002@pta2002.com>
2017-05-01 21:21:05 +0100

    Add LD plugin

---
+++ plugins/ld/ld.py
@@ -0,0 +1,23 @@
+import plugins.pluginapi
+import json
+
+# This is an example plugin. Feel free to remove it! :)
+class Plugin(plugins.pluginapi.BasicPlugin):
+    def __init__(self, bot, config):
+        name = 'ld'
+        desc = 'Find an LD game from the user'
+        plugins.pluginapi.BasicPlugin.__init__(self, name, desc, config, bot.connection)
+        self.register_command('find', self.cmd_find)
+        with open('ld38.json') as f:
+            self.ld_data = json.loads(f.read())
+
+    def cmd_find(self, usr, cmd, chan):
+        if len(cmd) > 1:
+            user_to_find = cmd[1]
+        else:
+            user_to_find = usr
+        for g in self.ld_data['games']:
+            if g['author'] == user_to_find:
+                self.send_msg("%s by %s: https://ldjam.com%s" % (g['name'], g['author'], g['path']), chan)
+                break
+        self.send_msg("Couldn't find any games by " + user_to_find, chan)
